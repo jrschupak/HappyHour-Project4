853683d322c01b0e077c737276c7e76b86001fec
Jonathan Schupak <jrschupak@gmail.com>
2016-05-02 15:41:34 -0400

    display conatiner and current coordinates onload

--- frontend/app/containers/App.js
+++ frontend/app/containers/App.js
@@ -1,12 +1,28 @@
 import React from 'react';
 import SearchButtonsContainer from './searchButtonsContainer';
+import DisplayContainer from './display';
 
 const App = React.createClass({
+
+  getInitialState: function() {
+    return {
+      currentLong: 0,
+      currentLat: 0
+    }
+  },
+
+  // Will get the current coordinates of your current location when the window loads.
+  componentWillMount(){
+    navigator.geolocation.getCurrentPosition(function(position) {
+      console.log(position.coords.latitude, position.coords.longitude);
+    })
+  },
   render: function() {
     return (
       <div className="app-container">
         <h1>Hello world</h1>
         <SearchButtonsContainer />
+        <DisplayContainer />
       </div>
 
     )
---
+++ frontend/app/containers/display.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const display = React.createClass({
+  render: function(){
+    return (
+      <div className='display-container'>
+        <p>THIS IS THE DISPLAY CONTAINER</p>
+      </div>
+    )
+  }
+});
+
+export default display;
