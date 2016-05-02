f4efd7e0cb307ee32a9825b9e87162f65bf9bbfc
Jonathan Schupak <jrschupak@gmail.com>
2016-05-02 11:52:04 -0400

    hello world

---
+++ frontend/app/containers/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const App = React.createClass({
+  render: function() {
+    return (
+      <div className="app-container">
+        <h1>Hello world</h1>
+      </div>
+
+    )
+  }
+})
+
+export default App;
